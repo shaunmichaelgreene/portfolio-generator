10ddb36b5b78ed6c0eb98235c133ece943dc2083
Shaun Greene <shaunmichaelgreene@gmail.com>
2022-05-26 21:48:46 -0400

    end 9.4.7, template updated

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const promptUser = () => {
       validate: nameInput => {
         if (nameInput) {
           return true;
-        } else {
+        } else { 
           console.log('Please enter your name!');
           return false;
         }
@@ -130,8 +130,10 @@ promptUser()
   .then(promptProject)
   .then(portfolioData => {
     const pageHTML = generatePage(portfolioData);
+
     fs.writeFile('./index.html', pageHTML, err => {
       if (err) throw new Error(err);
+
       console.log('Page created! Check out index.html in this directory to see it!');
     });
   });
